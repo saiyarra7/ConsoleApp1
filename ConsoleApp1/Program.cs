bcf418d6475b2d2661856afa18cd94e2561d2163
Sai <Sai@DELLLAPTOP>
2021-08-31 21:00:08 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            String[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+            double s = 0;
+            double n = 2.0;
+            s = (a + b + c) / n;
+            double Area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            Console.WriteLine(Area);
+        }
+    }
+}
